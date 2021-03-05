94850eba03d8152f7c23d408aa207017952dedbe
Nico <nicolas.bachleitner03@htl-salzburg.ac.at>
2021-03-05 16:34:08 +0100

    update

---
+++ Assets/widthheight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class widthheight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string screenWidth = Display.main.systemHeight.ToString();
+        string screenHeight = Display.main.systemWidth.ToString();
+
+        GetComponent<Text>().text = "Resolution : " + screenWidth + " X " + screenHeight;
+    }
+}
